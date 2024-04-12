ae8eb9cc23d9b27d87dc814c9d2b4eaadc1b8720
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-04-12 15:11:45 -0300

    Correções de Linguagem

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -9,7 +9,7 @@ export const Banner = () => {
             <div >
                 <p className='highlight'>SAMUEL RODRIGUES DA MOTA</p>
                 <h1 className='title lg'>Bem Vindo ao meu Portifólio</h1>
-                <p>Desenvolvedor Front-End em ascensão está pronto para transformar sua paixão por design e codificação em experiências de usuário excepcionais</p>
+                <p>Desenvolvedor Front-End em ascensão pronto para transformar minha paixão por design e codificação em experiências de usuário excepcionais</p>
                 <button onClick={() => { window.open('https://w.app/ZkLnuA', '_blank'); }}>Saiba mais</button>
             </div>
             <img src={notebook} alt="Vetor notebook" />
